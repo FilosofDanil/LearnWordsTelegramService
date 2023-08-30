5cda83d4377873734f1e8993236c0788a66e6c6d
FilosofDanil <serhio3347@gmail.com>
2023-08-30 20:58:08 +0200

    Added about

---
+++ telegramApi/src/main/java/com/example/telegramapi/components/impl/queries/AboutQueryHandler.java
@@ -0,0 +1,45 @@
+package com.example.telegramapi.components.impl.queries;
+
+import com.example.telegramapi.components.QueryHandler;
+import com.example.telegramapi.entities.UserRequest;
+import com.example.telegramapi.entities.UserSession;
+import com.example.telegramapi.enums.States;
+import com.example.telegramapi.services.ObtainTextService;
+import com.example.telegramapi.services.SessionService;
+import com.example.telegramapi.services.TelegramBotService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AboutQueryHandler implements QueryHandler {
+    private final SessionService sessionService;
+
+    private final ObtainTextService obtainTextService;
+
+    private final TelegramBotService telegramService;
+
+    @Override
+    public void handle(UserRequest request) {
+        UserSession session = sessionService.getSession(request.getChatId());
+        session = sessionService.checkUseData(session, request);
+        session.setState(States.ABOUT);
+        String lang = session.getUserData().getUserSettings().getInterfaceLang();
+        sessionService.saveSession(request.getChatId(), session);
+        telegramService.sendMessage(request.getChatId(), obtainTextService.read("About", lang));
+    }
+
+    @Override
+    public String getCallbackQuery(String lang) {
+        if (lang.equals("en")) {
+            return "ℹ️ About Bot";
+        } else {
+            return "ℹ️ Про Бота";
+        }
+    }
+
+    @Override
+    public boolean isInteger() {
+        return false;
+    }
+}
