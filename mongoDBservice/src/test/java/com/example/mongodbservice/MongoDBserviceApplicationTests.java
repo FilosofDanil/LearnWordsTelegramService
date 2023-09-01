35301d172fd8c930061a379437df8abd8710b788
FilosofDanil <serhio3347@gmail.com>
2023-09-01 18:19:45 +0200

    defined MongoDB microservice

---
+++ mongoDBservice/src/test/java/com/example/mongodbservice/MongoDBserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDBserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
