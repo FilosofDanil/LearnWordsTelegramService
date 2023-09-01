22f88e13824661b564a1f44963e66750e76091ea
FilosofDanil <serhio3347@gmail.com>
2023-09-01 19:16:51 +0200

    Defined basic Controller

---
+++ mongoDBservice/src/main/java/com/example/mongodbservice/controllers/TranslatedController.java
@@ -0,0 +1,12 @@
+package com.example.mongodbservice.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/map")
+public class TranslatedController {
+}
