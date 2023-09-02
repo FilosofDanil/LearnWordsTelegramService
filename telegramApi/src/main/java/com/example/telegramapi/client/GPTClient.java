ae37289adf04ac517644467ee17f8f95658187fd
FilosofDanil <serhio3347@gmail.com>
2023-09-02 22:37:53 +0200

    Defined GPTClient and GPTResponseEntity

--- telegramApi/src/main/java/com/example/telegramapi/client/GPTClient.java
+++ telegramApi/src/main/java/com/example/telegramapi/client/GPTClient.java
@@ -1,7 +1,13 @@
 package com.example.telegramapi.client;
 
+import com.example.telegramapi.entities.GPTResponseEntity;
+import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "dictionaryApi", url = "${application.config.dictionary-url}")
 public interface GPTClient {
+    @GetMapping("/translate/{text}")
+    GPTResponseEntity getMessage(@PathVariable("text") String text, @Param("langs") String langs);
 }
