5ebfe01651c2f95080e040aa0b31f8f280dc2b0b
FilosofDanil <serhio3347@gmail.com>
2023-09-08 14:50:51 +0200

    Defined Service and endpoint for get random word list.

---
+++ dictionaryApi/src/main/java/com/example/dictionaryapi/services/RandomTestServiceGenerator.java
@@ -0,0 +1,7 @@
+package com.example.dictionaryapi.services;
+
+import java.util.List;
+
+public interface RandomTestServiceGenerator {
+    List<String> formRandomService(Integer amount, String lang, String level);
+}
