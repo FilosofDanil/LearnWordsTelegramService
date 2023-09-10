17d4294eebe33783aa2dcf666b0ce08da5a4b048
FilosofDanil <serhio3347@gmail.com>
2023-09-10 19:01:39 +0200

    Added new State. Created /help command handler

---
+++ telegramApi/src/main/java/com/example/telegramapi/components/impl/commands/HelpCommandHandler.java
@@ -0,0 +1,43 @@
+package com.example.telegramapi.components.impl.commands;
+
+import com.example.telegramapi.components.RequestHandler;
+import com.example.telegramapi.components.additions.TestComponent;
+import com.example.telegramapi.entities.UserRequest;
+import com.example.telegramapi.entities.UserSession;
+import com.example.telegramapi.enums.States;
+import com.example.telegramapi.services.ObtainTextService;
+import com.example.telegramapi.services.SessionService;
+import com.example.telegramapi.services.bot.TelegramBotService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HelpCommandHandler extends RequestHandler {
+    private final SessionService sessionService;
+
+    private final TelegramBotService telegramService;
+
+    private final ObtainTextService obtainTextService;
+
+    private static final String command = "/test";
+
+    @Override
+    public boolean isApplicable(UserRequest request) {
+        return isCommand(request.getUpdate(), command);
+    }
+
+    @Override
+    public void handle(UserRequest request) {
+        UserSession session = sessionService.getSession(request.getChatId());
+        String lang = session.getUserData().getUserSettings().getInterfaceLang();
+        session.setState(States.HELP);
+        sessionService.saveSession(request.getChatId(), session);
+        telegramService.sendMessage(request.getChatId(), obtainTextService.read("helpText", lang));
+    }
+
+    @Override
+    public boolean isGlobal() {
+        return true;
+    }
+}
